ccb50982fffe321e99e54ef7029f496556d39532
Carlos <carlosaac232001@gmail.com>
2026-02-24 10:46:31 -0500

    feat: create and style add car form with custom components

--- src/pages/AddCar.tsx
+++ src/pages/AddCar.tsx
@@ -1,29 +1,64 @@
+import Footer from '@/components/Footer';
+import Form from '@/components/Form';
+import Header from '@/components/Header';
+import ImagePreviewInput from '@/components/ImagePreviewInput';
+import Input from '@/components/Input';
+import SpinnerButton from '@/components/SpinnerButton';
+import { Button } from '@/components/ui/button';
 import { useAddCar } from '@/hooks/useAddCar';
 export default function AddCarForm() {
-  const { handleSubmit } = useAddCar();
+  const { isLoading, handleSubmit } = useAddCar();
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='model'>Model</label>
-        <input className='border' type='text' name='model' id='model' />
+    <main className='flex min-h-screen flex-col'>
+      <Header />
 
-        <label htmlFor='brand'>Brand</label>
-        <input className='border' type='text' name='brand' id='brand' />
+      <h1 className='my-6 text-center text-2xl font-bold'>Add Car</h1>
 
-        <label htmlFor='color'>Color</label>
-        <input className='border' type='text' name='color' id='color' />
+      <Form onSubmit={handleSubmit}>
+        <Input
+          label='Model'
+          type='text'
+          name='model'
+          id='model'
+          placeholder='Corolla'
+          required={true}
+        />
+        <Input
+          label='Brand'
+          type='text'
+          name='brand'
+          id='brand'
+          placeholder='Toyota'
+          required={true}
+        />
+        <Input
+          label='Color'
+          type='text'
+          name='color'
+          id='color'
+          placeholder='White'
+          required={true}
+        />
+        <Input
+          label='Year'
+          type='number'
+          name='year'
+          id='year'
+          placeholder='2007'
+          required={true}
+        />
+        <ImagePreviewInput />
 
-        <label htmlFor='year'>Year</label>
-        <input className='border' type='number' name='year' id='year' />
+        <Button
+          className='mt-4 border bg-neutral-950 font-bold text-neutral-50 uppercase shadow-sm transition-transform duration-150 ease-out hover:cursor-pointer active:scale-97'
+          type='submit'
+        >
+          {isLoading ? <SpinnerButton text='Adding...' /> : 'Add'}
+        </Button>
+      </Form>
 
-        <label htmlFor='photo'>Photo</label>
-        <input className='border' type='text' name='photo' id='photo' />
-
-        <button className='border' type='submit'>
-          Add
-        </button>
-      </form>
-    </>
+      <Footer />
+    </main>
   );
 }

4d1e02a17d487192fb62358365422176274bee0e
Carlos <carlosaac232001@gmail.com>
2026-02-24 18:49:16 -0500

    chore: clean component and use custom components

--- src/pages/AddCar.tsx
+++ src/pages/AddCar.tsx
@@ -1,18 +1,13 @@
-import Footer from '@/components/Footer';
+import BlurButton from '@/components/BlurButton';
 import Form from '@/components/Form';
-import Header from '@/components/Header';
 import ImagePreviewInput from '@/components/ImagePreviewInput';
 import Input from '@/components/Input';
-import SpinnerButton from '@/components/SpinnerButton';
-import { Button } from '@/components/ui/button';
 import { useAddCar } from '@/hooks/useAddCar';
 export default function AddCarForm() {
   const { isLoading, handleSubmit } = useAddCar();
 
   return (
-    <main className='flex min-h-screen flex-col'>
-      <Header />
-
+    <>
       <h1 className='my-6 text-center text-2xl font-bold'>Add Car</h1>
 
       <Form onSubmit={handleSubmit}>
@@ -50,15 +45,12 @@ export default function AddCarForm() {
         />
         <ImagePreviewInput />
 
-        <Button
-          className='mt-4 border bg-neutral-950 font-bold text-neutral-50 uppercase shadow-sm transition-transform duration-150 ease-out hover:cursor-pointer active:scale-97'
-          type='submit'
-        >
-          {isLoading ? <SpinnerButton text='Adding...' /> : 'Add'}
-        </Button>
+        <BlurButton
+          isLoading={isLoading}
+          staticMessage='Add'
+          promiseMessage='Adding...'
+        />
       </Form>
-
-      <Footer />
-    </main>
+    </>
   );
 }

1573756343602786511f24cba44c430627558f0c
Carlos <carlosaac232001@gmail.com>
2026-02-24 18:39:08 -0500

    feat: add header links to different pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,16 @@ export default function Header() {
         <h1 className='text-2xl font-bold uppercase'>Car Inventory</h1>
       </Link>
 
-      <nav>
-        <Link to='/'>Home</Link>
+      <nav className='flex gap-3'>
+        <Link className='hover:underline hover:underline-offset-2' to='/'>
+          Home
+        </Link>
+        <Link className='hover:underline hover:underline-offset-2' to='/cars'>
+          Cars Inventory
+        </Link>
+        <Link className='hover:underline hover:underline-offset-2' to='/add'>
+          Add Car
+        </Link>
       </nav>
     </header>
   );

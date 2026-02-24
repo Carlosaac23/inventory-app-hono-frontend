805fb060b82c24d34e8f34fbffc55df9a5fc9da4
Carlos <carlosaac232001@gmail.com>
2026-02-24 18:40:36 -0500

    chore: use custom hook and InfoDiv component to clean the component

--- src/components/ImagePreviewInput.tsx
+++ src/components/ImagePreviewInput.tsx
@@ -1,28 +1,13 @@
-import { useState, useEffect } from 'react';
+import { SearchX } from 'lucide-react';
 
+import { useImagePreview } from '@/hooks/useImagePreview';
+
+import InfoDiv from './InfoDiv';
 import Input from './Input';
 
 export default function ImagePreviewInput() {
-  const [imageUrl, setImageUrl] = useState('');
-  const [debouncedImageUrl, setDebouncedImageUrl] = useState('');
-  const [hasError, setHasError] = useState(false);
-
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setImageUrl(e.target.value);
-
-    if (!e.target.value) {
-      setDebouncedImageUrl('');
-    }
-  };
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setDebouncedImageUrl(imageUrl);
-      setHasError(false);
-    }, 500);
-
-    return () => clearTimeout(timer);
-  }, [imageUrl]);
+  const { debouncedImageUrl, hasError, setHasError, handleOnChange } =
+    useImagePreview();
 
   return (
     <>
@@ -38,9 +23,11 @@ export default function ImagePreviewInput() {
         <p className='mb-2 font-semibold'>Preview:</p>
         {debouncedImageUrl &&
           (hasError ? (
-            <p className='rounded-sm border border-red-400 bg-red-50 p-2 text-center font-medium text-red-500 capitalize'>
-              Error loading image
-            </p>
+            <InfoDiv
+              message='URL not found'
+              isError={true}
+              icon={<SearchX />}
+            />
           ) : (
             <img
               className='mx-auto h-60 rounded-md object-contain shadow-md'

c616d41c46eb5f146673884451ef12f5ea4cc4a0
Carlos <carlosaac232001@gmail.com>
2026-02-24 10:49:48 -0500

    feat: create types for Input and Form

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,18 @@ export interface Car {
   car_year: number;
   car_photo: string;
 }
+
+export interface Input {
+  label: string;
+  type: string;
+  name: string;
+  id: string;
+  placeholder: string;
+  required?: boolean;
+  onChange?: (e: any) => void;
+}
+
+export interface Form {
+  children: React.ReactNode;
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
+}

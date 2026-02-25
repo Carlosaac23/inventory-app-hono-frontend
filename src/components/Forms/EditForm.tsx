e93738e4b5e467fc5dc618594afb957e1dc0a022
Carlos <carlosaac232001@gmail.com>
2026-02-25 17:28:28 -0500

    feat: move all react-hook-form to custom logic and return important variables

--- src/components/Forms/EditForm.tsx
+++ src/components/Forms/EditForm.tsx
@@ -6,66 +6,97 @@ import BlurButton from '@/components/BlurButton';
 import Form from '@/components/Form';
 import InfoDiv from '@/components/InfoDiv';
 import Input from '@/components/Input';
-import { useEditCar } from '@/hooks/useEditCar';
+import { useEditCarForm } from '@/hooks/useEditCarForm';
 import editCarQueryOption from '@/queryOptions/editCarQueryOption';
 
 export default function EditForm() {
   const { carId } = useParams();
-  const { isLoading, handleSubmit } = useEditCar();
   const {
     data: car,
     isPending,
     isError,
     error
   } = useQuery(editCarQueryOption(carId));
+  const { register, errors, onSubmit, isLoading } = useEditCarForm(car);
 
+  if (!car) return <InfoDiv message='Car does not exist' isError={true} />;
   if (isPending) return <InfoDiv message='Loading...' isError={false} />;
   if (isError)
     return (
       <InfoDiv message={error.message} isError={true} icon={<ShieldAlert />} />
     );
 
-  const { car_model, car_brand, car_color, car_year, car_photo } = car;
+  const { car_model, car_photo } = car;
 
   return (
     <>
       <h1 className='my-6 text-center text-2xl font-bold'>Edit Car</h1>
 
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={onSubmit}>
         <Input
           label='Model'
           type='text'
           name='model'
           id='model'
-          defaultValue={car_model}
+          register={register}
+          errors={errors}
+          rules={{
+            required: 'Model is required',
+            minLength: {
+              value: 2,
+              message: 'Model must be at least 2 characters'
+            }
+          }}
         />
         <Input
           label='Brand'
           type='text'
           name='brand'
           id='brand'
-          defaultValue={car_brand}
+          register={register}
+          errors={errors}
+          rules={{
+            required: 'Brand is required',
+            pattern: {
+              value: /^[a-zA-Z\s-]+$/,
+              message: 'Brand can only contain letters, spaces, and hyphens'
+            }
+          }}
         />
         <Input
           label='Color'
           type='text'
           name='color'
           id='color'
-          defaultValue={car_color}
+          register={register}
+          errors={errors}
+          rules={{ required: 'Color is required' }}
         />
         <Input
           label='Year'
           type='text'
           name='year'
           id='year'
-          defaultValue={car_year}
+          register={register}
+          errors={errors}
+          rules={{
+            required: 'Year is required',
+            valueAsNumber: true,
+            min: { value: 1886, message: 'Year must be 1886 or newer' },
+            max: {
+              value: new Date().getFullYear() + 1,
+              message: 'Years is too far in the future'
+            }
+          }}
         />
         <Input
           label='Photo'
           type='text'
           name='photo'
           id='photo'
-          defaultValue={car_photo}
+          register={register}
+          errors={errors}
+          rules={{ required: 'URL is required' }}
         />
 
         <h2 className='mb-2 font-semibold'>Current photo:</h2>

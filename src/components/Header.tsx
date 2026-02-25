295b9db7cdbdafbd0a298448b0096a98f9efe8a9
Carlos <carlosaac232001@gmail.com>
2026-02-25 17:31:11 -0500

    refactor: make the header dynamic by render buttons from object array to prevent repetitive code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
 import { Link } from 'react-router';
 
+import type { HeaderButton } from '@/types';
+
+import { Button } from '@/components/ui/button';
+import { buttons } from '@/lib/buttons';
 export default function Header() {
   return (
     <header className='flex justify-between bg-neutral-300 p-6'>
@@ -7,16 +11,16 @@ export default function Header() {
         <h1 className='font-bold uppercase md:text-3xl'>Car Inventory</h1>
       </Link>
 
-      <nav className='flex gap-1 text-sm md:gap-3 md:text-lg'>
-        <Link className='hover:underline hover:underline-offset-2' to='/'>
-          Home
-        </Link>
-        <Link className='hover:underline hover:underline-offset-2' to='/cars'>
-          Cars Inventory
-        </Link>
-        <Link className='hover:underline hover:underline-offset-2' to='/add'>
-          Add Car
-        </Link>
+      <nav className='flex gap-1'>
+        {buttons.map((button: HeaderButton) => (
+          <>
+            <Link to={button.path}>
+              <Button className='text-sm md:gap-3 md:text-lg' variant='ghost'>
+                {button.text}
+              </Button>
+            </Link>
+          </>
+        ))}
       </nav>
     </header>
   );

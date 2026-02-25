631d60d7864b190b4b139241807c2e10e202ddc3
Carlos <carlosaac232001@gmail.com>
2026-02-25 15:42:07 -0500

    style: add responsiveness style

--- src/components/Cars.tsx
+++ src/components/Cars.tsx
@@ -1,5 +1,4 @@
 import { useQuery } from '@tanstack/react-query';
-import { Link } from 'react-router';
 
 import type { Car } from '@/types/index';
 
@@ -15,16 +14,18 @@ export default function Cars() {
 
   return (
     <>
-      <h1>Cars</h1>
-      <div className='mx-8 flex gap-4'>
+      <h1 className='my-5 text-center text-3xl font-semibold'>
+        Cars Inventory
+      </h1>
+      <div
+        className='mx-auto grid max-w-7xl gap-6 px-4 py-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'
+      >
         {isPending ? (
           <Spinner className='mx-auto my-10 size-8 text-neutral-400' />
         ) : (
           cars?.map((car: Car) => <Card key={car.car_id} {...car} />)
         )}
       </div>
-
-      <Link to='/add'>Add car</Link>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ export default function Header() {
   return (
     <header className='flex justify-between bg-neutral-300 p-6'>
       <Link to='/'>
-        <h1 className='text-2xl font-bold uppercase'>Car Inventory</h1>
+        <h1 className='font-bold uppercase md:text-3xl'>Car Inventory</h1>
       </Link>
 
-      <nav className='flex gap-3'>
+      <nav className='flex gap-1 text-sm md:gap-3 md:text-lg'>
         <Link className='hover:underline hover:underline-offset-2' to='/'>
           Home
         </Link>
